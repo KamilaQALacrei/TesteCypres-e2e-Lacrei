3230b1bc512672c71b53eed30416828955d46f16
Kamila <137840707+KamilaQALacrei@users.noreply.github.com>
2023-06-28 19:19:25 +0100

    cy

--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -1,5 +1,15 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+///<reference types="Cypress"/>
+
+beforeEach(() => {
+  cy.visit('https://lacrei-pessoa-profissional.vercel.app/')
+});
+
+
+describe('Funcionalidade Botão de criar conta profissional', () => {
+  it('Cenário 1: Criar conta', () => {
+    
+    cy.get('#input\:c375ee802cc96ecf4fdc9c88d3c51216').type("kamila1dsg@gmail.com")
+    cy.get('#input\:e168be6f4b2e75b4159beeed64a0b443').type("K123456")
+    cy.get('.sc-18p54ra-0 > .sc-18p54ra-1').click()
+  });
 })
