94272418bca2eea725b9f553765e4e3e33255cf9
Kamila <137840707+KamilaQALacrei@users.noreply.github.com>
2023-06-29 19:26:12 +0100

    cy

--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -1,15 +1,48 @@
 ///<reference types="Cypress"/>
 
-beforeEach(() => {
-  cy.visit('https://lacrei-pessoa-profissional.vercel.app/')
-});
 
 
-describe('Funcionalidade Botão de criar conta profissional', () => {
-  it('Cenário 1: Criar conta', () => {
-    
-    cy.get('#input\:c375ee802cc96ecf4fdc9c88d3c51216').type("kamila1dsg@gmail.com")
-    cy.get('#input\:e168be6f4b2e75b4159beeed64a0b443').type("K123456")
-    cy.get('.sc-18p54ra-0 > .sc-18p54ra-1').click()
+
+
+describe('Funcionalidade botão criar conta profissional', () => {
+  beforeEach(() => {
+    cy.visit('https://lacrei-pessoa-profissional.vercel.app/');
+    cy.clearCookies() 
   });
-})
+  
+
+  it('Cenário 1:Criar conta', () => {
+  
+    cy.get('.sc-18p54ra-0 > .sc-18p54ra-1').click()
+    cy.get(':nth-child(2) > .sc-18p54ra-1').click()
+    cy.contains("Selecione o Estado").click()
+    cy.get("#option-SP").click()
+    cy.get(':nth-child(2) > .sc-18p54ra-1').click()
+    cy.get(':nth-child(1) > .sc-16me5u5-2').click()
+    cy.get(':nth-child(1) > .sc-16me5u5-2').type("kamila")
+    cy.get(':nth-child(2) > .sc-16me5u5-2').click()
+    cy.get(':nth-child(2) > .sc-16me5u5-2').type("kamila1@gmail.com")
+    cy.contains("Selecione sua Profissão").click()
+    cy.get("#option-1").click()
+    cy.get(':nth-child(4) > .sc-16me5u5-2').click()
+    cy.get(':nth-child(4) > .sc-16me5u5-2').type("1234")
+    cy.get(':nth-child(5) > .sc-16me5u5-2').click()
+    cy.get(':nth-child(5) > .sc-16me5u5-2').type("12345Kab#")
+    cy.get(':nth-child(6) > .sc-16me5u5-2').click()
+    cy.get(':nth-child(6) > .sc-16me5u5-2').type("12345Kab#")
+    cy.get("span[role='checkbox']").click()
+    cy.get(':nth-child(1) > .sc-18p54ra-1').click()
+    cy.xpath('/html/body/div[1]/div[1]/div/section/div[1]/form/div[2]/a/button[1]').click()
+    
+
+    
+    cy.get('.sc-2bp86y-2 > p').should('contain', 'Antes de continuar, verifique se as informações estão corretas.')
+    cy.get(':nth-child(1) > .sc-16me5u5-1').should('contain', 'Nome Civil ou Social completo')
+    cy.get('p').should('contain', 'E-mail')
+   
+ 
+  
+    
+   
+    });
+  });
