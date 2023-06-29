0083dbaab0c773e651765f19fd4bf1fe62a4f6cd
Kamila <137840707+KamilaQALacrei@users.noreply.github.com>
2023-06-29 20:15:17 +0100

    ip

--- cypress/e2e/paniel.cy.js
+++ cypress/e2e/paniel.cy.js
@@ -16,9 +16,9 @@
         cy.get(':nth-child(2) > .sc-16me5u5-2').type("Kamila36#")
         cy.get('[type="submit"]').click()
 
-        cy.get('h1').should('contain', 'Boas.vindas à Lacrei Saúde') 
+        cy.get('h1').should('contain', 'Boas-vindas à Lacrei Saúde') 
         cy.get('.forgot-password').should('contain', 'Esqueci minha senha')  
-    
+        cy.get('p').should('contain', 'Entre ou crie sua conta Lacrei Saúde.')  
         
           
          
