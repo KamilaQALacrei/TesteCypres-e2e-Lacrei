57cf98d7e68ef6dfd4f43a0ad50d0196fdfad192
Kamila <137840707+KamilaQALacrei@users.noreply.github.com>
2023-06-29 20:02:05 +0100

    cy

--- cypress/e2e/paniel.cy.js
+++ cypress/e2e/paniel.cy.js
@@ -12,6 +12,9 @@
         it('Cenário 1: Acessar página de cadastro', () => {
         cy.get(':nth-child(1) > .sc-16me5u5-2').click()
         cy.get(':nth-child(1) > .sc-16me5u5-2').type("kamiladsg36@gmail.com")
+        cy.get(':nth-child(2) > .sc-16me5u5-2').click()
+        cy.get(':nth-child(2) > .sc-16me5u5-2').type("Kamila36#")
+        cy.get('[type="submit"]').click()
         
           
     
