590442dd0337d3827ce62a9ee59b6d8a3ccdda87
Kamila <137840707+KamilaQALacrei@users.noreply.github.com>
2023-06-29 20:29:31 +0100

    ok

--- cypress/e2e/paniel.cy.js
+++ cypress/e2e/paniel.cy.js
@@ -11,9 +11,9 @@
       
         it('Cenário 1: Acessar página de cadastro', () => {
         cy.get(':nth-child(1) > .sc-16me5u5-2').click()
-        cy.get(':nth-child(1) > .sc-16me5u5-2').type("kamiladsg36@gmail.com")
+        cy.get(':nth-child(1) > .sc-16me5u5-2').type("kamiladsg@gmail.com")
         cy.get(':nth-child(2) > .sc-16me5u5-2').click()
-        cy.get(':nth-child(2) > .sc-16me5u5-2').type("Kamila36#")
+        cy.get(':nth-child(2) > .sc-16me5u5-2').type("Kamila123%")
         cy.get('[type="submit"]').click()
 
         cy.get('h1').should('contain', 'Boas-vindas à Lacrei Saúde') 
@@ -24,7 +24,10 @@
         cy.get('.sc-18p54ra-0 > .sc-18p54ra-1').should('contain', 'Criar conta') 
         cy.get('.forgot-password').should('contain', 'Esqueci minha senha')  
          
-          
-         
+        
+          });
+
+          it('Cenário 2: Acessar página de diversidade ', () => {
+            
           });
         });
