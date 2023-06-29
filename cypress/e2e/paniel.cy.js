4a4146c4286566b0af3f4ea921ee68875de6422a
Kamila <137840707+KamilaQALacrei@users.noreply.github.com>
2023-06-29 21:23:43 +0100

    up

--- cypress/e2e/paniel.cy.js
+++ cypress/e2e/paniel.cy.js
@@ -10,24 +10,26 @@
         
       
         it('Cenário 1: Acessar página de cadastro', () => {
-        cy.get(':nth-child(1) > .sc-16me5u5-2').click()
-        cy.get(':nth-child(1) > .sc-16me5u5-2').type("kamiladsg@gmail.com")
-        cy.get(':nth-child(2) > .sc-16me5u5-2').click()
-        cy.get(':nth-child(2) > .sc-16me5u5-2').type("Kamila123%")
-        cy.get('[type="submit"]').click()
+            cy.get(':nth-child(1) > .sc-16me5u5-2').click()
+            cy.get(':nth-child(1) > .sc-16me5u5-2').type("chriscsantos.qa@gmail.com")
+            cy.get(':nth-child(2) > .sc-16me5u5-2').click()
+            cy.get(':nth-child(2) > .sc-16me5u5-2').type("Qa123#45")
+            cy.get('[type="submit"]').click()
 
-        cy.get('h1').should('contain', 'Boas-vindas à Lacrei Saúde') 
-        cy.get('p').should('contain', 'Entre ou crie sua conta Lacrei Saúde.')  
-        cy.get(':nth-child(1) > .sc-16me5u5-1').should('contain', 'E-mail') 
-        cy.get(':nth-child(2) > .sc-16me5u5-1').should('contain', 'Senha')  
-        cy.get('[type="submit"]').should('contain', 'Entrar') 
-        cy.get('.sc-18p54ra-0 > .sc-18p54ra-1').should('contain', 'Criar conta') 
-        cy.get('.forgot-password').should('contain', 'Esqueci minha senha')  
-         
+
+            cy.get('h1').should('contain', 'Boas-vindas à Lacrei Saúde') 
+            cy.get('p').should('contain', 'Entre ou crie sua conta Lacrei Saúde.')  
+            cy.get(':nth-child(1) > .sc-16me5u5-1').should('contain', 'E-mail') 
+            cy.get(':nth-child(2) > .sc-16me5u5-1').should('contain', 'Senha')  
+            cy.get('[type="submit"]').should('contain', 'Entrar') 
+            cy.get('.sc-18p54ra-0 > .sc-18p54ra-1').should('contain', 'Criar conta') 
+            cy.get('.forgot-password').should('contain', 'Esqueci minha senha')  
         
-          });
 
-          it('Cenário 2: Acessar página de diversidade ', () => {
-            
-          });
+            cy.get(':nth-child(2) > .sc-16wss1z-3').click()
+            cy.get('[href="/saude/painel-inicial"] > .sc-18p54ra-1').click()
+        });
+
+
+
         });
--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -32,13 +32,23 @@ describe('Funcionalidade botão criar conta profissional', () => {
     cy.get(':nth-child(6) > .sc-16me5u5-2').type("12345Kab#")
     cy.get("span[role='checkbox']").click()
     cy.get(':nth-child(1) > .sc-18p54ra-1').click()
-    cy.xpath('/html/body/div[1]/div[1]/div/section/div[1]/form/div[2]/a/button[1]').click()
+   
     
 
     
-    cy.get('.sc-2bp86y-2 > p').should('contain', 'Antes de continuar, verifique se as informações estão corretas.')
+    cy.get('h1').should('contain', 'Informe o Estado onde você atua')
+    cy.get('.placeholder').should('contain', 'Selecione o Estado')
+    cy.get('#label\:7b06c01432be128d5f871c46b70bf6c9').should('contain', 'Estado')
     cy.get(':nth-child(1) > .sc-16me5u5-1').should('contain', 'Nome Civil ou Social completo')
-    cy.get('p').should('contain', 'E-mail')
+    cy.get(':nth-child(2) > .sc-16me5u5-1').should('contain', 'E-mail')
+    cy.get('#label\:f419811a712070974c8f6847acea135a').should('contain', 'Profissão')
+    cy.get(':nth-child(4) > .sc-16me5u5-1').should('contain', 'Número de Inscrição Profissional')
+    cy.get(':nth-child(5) > .sc-16me5u5-1').should('contain', 'Senha')
+    cy.get(':nth-child(6) > .sc-16me5u5-1').should('contain', 'Confirmar senha')
+    cy.get('.sc-2bp86y-2 > p').should('contain', 'Antes de continuar, verifique se as informações estão corretas.')
+
+
+
    
  
   
