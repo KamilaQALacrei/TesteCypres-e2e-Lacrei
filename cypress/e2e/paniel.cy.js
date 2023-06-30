c8ab246bac43760a18b47830a12eeefedc270bdd
Kamila <137840707+KamilaQALacrei@users.noreply.github.com>
2023-06-30 10:51:57 +0100

    up

--- cypress/e2e/paniel.cy.js
+++ cypress/e2e/paniel.cy.js
@@ -15,19 +15,24 @@
             cy.get(':nth-child(2) > .sc-16me5u5-2').click()
             cy.get(':nth-child(2) > .sc-16me5u5-2').type("Qa123#45")
             cy.get('[type="submit"]').click()
+            cy.get(':nth-child(2) > .sc-16wss1z-3').click()
+            cy.get('[href="/saude/painel-inicial"] > .sc-18p54ra-1').click()
+            cy.get(':nth-child(1) > .sc-16wss1z-3').click()
+            cy.get('.sc-18p54ra-0 > .sc-18p54ra-1').click()
 
 
             cy.get('h1').should('contain', 'Boas-vindas à Lacrei Saúde') 
-            cy.get('p').should('contain', 'Entre ou crie sua conta Lacrei Saúde.')  
-            cy.get(':nth-child(1) > .sc-16me5u5-1').should('contain', 'E-mail') 
+            cy.get('p').should('contain', 'Entre ou crie sua conta Lacrei Saúde.')
+            cy.get(':nth-child(1) > .sc-16me5u5-1').should('contain', 'E-mail')
             cy.get(':nth-child(2) > .sc-16me5u5-1').should('contain', 'Senha')  
             cy.get('[type="submit"]').should('contain', 'Entrar') 
             cy.get('.sc-18p54ra-0 > .sc-18p54ra-1').should('contain', 'Criar conta') 
             cy.get('.forgot-password').should('contain', 'Esqueci minha senha')  
         
+        });
 
-            cy.get(':nth-child(2) > .sc-16wss1z-3').click()
-            cy.get('[href="/saude/painel-inicial"] > .sc-18p54ra-1').click()
+        it('Cenário 2: Acessar página de diversidade', () => {
+          cy.get(':nth-child(2) > .sc-16wss1z-3').click()
         });
 
 
