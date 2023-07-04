e1ee96af4595c416d67d61ca79f47b2b2b2417cc
Kamila <137840707+KamilaQALacrei@users.noreply.github.com>
2023-07-04 11:28:38 +0100

    Update paniel.cy.js

--- cypress/e2e/paniel.cy.js
+++ cypress/e2e/paniel.cy.js
@@ -35,7 +35,7 @@
           cy.get('[type="submit"]').should('contain', 'Entrar') 
           cy.get('.sc-18p54ra-0 > .sc-18p54ra-1').should('contain', 'Criar conta') 
           cy.get('.forgot-password').should('contain', 'Esqueci minha senha') 
-          cy.get('.boxP > p').should('contain', 'Você faz a diferença para milhares de pessoas LGBTQIAPN+ que buscam inclusão, segurança e acolhimento nos serviços de saúde.') 
+          cy.get('.boxP > p').should('contain', 'Você faz a diferença para milhares de pessoas LGBTQIAPN+ que buscam inclusão, segurança e acolhimento nos serviços de saúde.')
           cy.get(':nth-child(1) > .sc-16wss1z-1 > .sc-16wss1z-2').should('contain', 'Clique no botão “Cadastrar” para enviar seu cadastro') 
           cy.get(':nth-child(2) > .sc-16wss1z-1 > .sc-16wss1z-2').should('contain', 'Edite seu perfil com dados de diversidade') 
           cy.get(':nth-child(3) > .sc-16wss1z-1 > .sc-16wss1z-2').should('contain', 'Acesse a área de Segurança e Privacidade') 
